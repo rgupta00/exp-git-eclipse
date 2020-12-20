d63aa1174ba7c350bfe88ea4fb7ca63c082fd942
rgupta00 <rgupta.mtech@gmail.com>
2020-12-20 17:35:14 +0530

    initial commit

---
+++ mvc/src/main/java/com/demo/HelloController.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/")
+	public ModelAndView hello(ModelAndView mv) {
+		mv.addObject("msg", "hello to spring mvc");
+		mv.setViewName("hello");
+		return mv;
+	}
+}
